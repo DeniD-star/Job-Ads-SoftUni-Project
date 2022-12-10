4adfb8cab1b79da4434993fde2ad965dc742c5f8
DeniD-star <dimitrova.denise@gmail.com>
2022-12-10 12:25:57 +0100

    validation ad-model

--- models/Ad.js
+++ models/Ad.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
-    headline: { type: String, required: true },
-    location: { type: String, required: true },
-    companyName: { type: String, required: true },
-    descriptionCompany: { type: String, required: true },
+    headline: { type: String, required: [true, 'Headline is required'], minLength: [4, 'Headline has to be at least 4 characters long!'] },
+    location: { type: String, required: [true, 'Location is required'], minLength: [8, 'Location has to be at least 8 characters long!'] },
+    companyName: { type: String, required: [true, 'Name of company is required'], minLength: [3, 'Company name has to be at least 3 characters long!'] },
+    descriptionCompany: { type: String, required: [true, 'Description is required'], maxLength: [40, 'Description cannot be more than 40 characters long!'] },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
     usersApplied: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }]
 })
